ab38f701f9680243178745bef631544ece78fd4e
Nahuel Andreoli <andreoli.nahuel@gmail.com>
2018-03-06 20:11:34 -0300

    fixed emojis not appearing properly

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     lazy var concentrationGame = Concentration(numberOfPairsOfCards: cardButtonCollection.count / 2)
     
     @IBOutlet var cardButtonCollection: [UIButton]!
-    var emojiDictionary = [0:"😀"]
+    var emojiDictionary = [1:"😀"]
     
     @IBAction func touchCard(_ sender: UIButton) {
         let indexOfCard = cardButtonCollection.index(of: sender)!
@@ -25,18 +25,18 @@ class ViewController: UIViewController {
         for cardButtonIndex in cardButtonCollection.indices{
             let card = concentrationGame.cardCollection[cardButtonIndex]
             let cardButton = cardButtonCollection[cardButtonIndex]
-            updateButtonFromCard(cardButton, card, cardButtonIndex)
+            updateButtonFromCard(cardButton, card, card.identifier)
         }
     }
     
-    func updateButtonFromCard(_ cardButton: UIButton, _ card: Card, _ cardButtonIndex: Int){
+    func updateButtonFromCard(_ cardButton: UIButton, _ card: Card, _ cardIndex: Int){
         if(card.isMatched){
             cardButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
             cardButton.setTitle("", for: UIControlState.normal)
         } else {
             if(card.isFaceUp){
                 cardButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-                cardButton.setTitle(emojiDictionary[cardButtonIndex], for: UIControlState.normal)
+                cardButton.setTitle(emojiDictionary[cardIndex], for: UIControlState.normal)
             } else {
                 cardButton.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
             }
